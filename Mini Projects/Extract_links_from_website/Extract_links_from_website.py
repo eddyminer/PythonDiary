c508d1bf7ecb8911f1f05ab3f50864d4c240a2d7
eddyminer <65339139+eddyminer@users.noreply.github.com>
2021-06-06 18:14:29 +0800

    Add files via upload

---
+++ Mini Projects/Extract_links_from_website/Extract_links_from_website.py
@@ -0,0 +1,25 @@
+import requests as rq
+from bs4 import BeautifulSoup
+
+url = input("Enter Link: ")
+#the url must contain 'https' or 'http' for rq to function
+if ("https" or "http") in url:
+    data = rq.get(url)
+else:
+    data = rq.get("https://" + url)
+
+#parse HTML code obtained
+#HTML parsing is basically taking in HTML code
+#and extracting relevant information
+soup = BeautifulSoup(data.text, "html.parser")
+links = []
+
+#find all <a> attribute in HTML code with href attribute
+#href attribute specifies the URL of the page the link goes to
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
+    print(link.get("href"))
+    
+#some people use urllib2.urlopen to replace rq
+#in this case, the text 'html.parser' will be removed from line 14
+#overall both libraries work the same way
